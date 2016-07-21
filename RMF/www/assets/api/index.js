bb2432410d55ff758a99c39bb4de75879c1082fb
SpiffySaxMan <willbennett822@gmail.com>
2016-07-21 16:49:15 -0500

    Made API version 0.XX currently 0.2

--- RMF/www/assets/api/index.js
+++ RMF/www/assets/api/index.js
@@ -1,38 +1,25 @@
 var stormpath = require('stormpath')
 var app;
 var util = require ('util');
+var appHref = 'https://api.stormpath.com/v1/applications/KzUPEioLybUfu2YwUjb8o';
+var client;
 
 // Find the user's home directory (works on both Windows and *nix):
 var home = process.env[(process.platform === 'win32' ? 'USERPROFILE' : 'HOME')];
 var apiKeyFilePath = home + '/.stormpath/apiKey.properties';
 
-// Will be available after the properties file is asynchronously loaded from
-// disk:
-var client;
-
-console.log("before:");
-
-var apiKey = new stormpath.ApiKey(
-  process.env['STORMPATH_CLIENT_APIKEY_ID'],
-  process.env['STORMPATH_CLIENT_APIKEY_SECRET']
-);
-
-var client = new stormpath.Client({ apiKey: apiKey });
-
-console.log("after:");
-
-var appHref = 'https://api.stormpath.com/v1/applications/KzUPEioLybUfu2YwUjb8o';
-
-client.getApplication(appHref, function(err, app) {
-  if (err) throw err;
-  console.log(app);
-});
-
-client.getDirectories({ expand: 'groups' }, function(err, dirs) {
-  dirs.each(function(dir, cb) {
-    console.log(dir);
-    cb();
-  }, function(err) {
-    console.log('Finished iterating over directories.');
+console.log("before");
+if (apiKeyFilePath != 'undefined') {
+  stormpath.loadApiKey(apiKeyFilePath, function apiKeyFileLoaded(err, apiKey) {
+    client = new stormpath.Client({ apiKey: apiKey });
   });
-});
+}
+
+else {
+  var apiKey = new stormpath.ApiKey(
+    process.env['STORMPATH_CLIENT_APIKEY_ID'],
+    process.env['STORMPATH_CLIENT_APIKEY_SECRET']
+  );
+  var client = new stormpath.Client({ apiKey: apiKey });
+}
+console.log("after");
